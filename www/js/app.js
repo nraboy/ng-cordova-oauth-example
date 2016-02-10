f6008c10b92734ae984853bbdcd714e7bee086a4
Nic Raboy <nraboy@gmail.com>
2016-02-10 08:28:41 -0600

    update to latest ionic

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,26 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordovaOauth'])
 
 .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if(window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 })
 
 .controller("ExampleController", function($scope, $cordovaOauth) {
@@ -30,7 +42,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth'])
     }
 
     $scope.facebookLogin = function() {
-        $cordovaOauth.facebook("APP_ID_HERE", ["email"], {"auth_type": "rerequest"}).then(function(result) {
+        $cordovaOauth.facebook("CLIENT_ID_HERE", ["email"], {"auth_type": "rerequest"}).then(function(result) {
             console.log(JSON.stringify(result));
         }, function(error) {
             console.log(JSON.stringify(error));
